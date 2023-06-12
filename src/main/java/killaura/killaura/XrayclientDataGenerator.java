ba92c4d9181c52b5f993585399cbb515cb47a216
SCP-939-83 <512707@vistacollege.nl>
2023-06-12 10:47:36 +0200

    ee

---
+++ src/main/java/killaura/killaura/XrayclientDataGenerator.java
@@ -0,0 +1,10 @@
+package killaura.killaura;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+public class XrayclientDataGenerator implements DataGeneratorEntrypoint  {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
