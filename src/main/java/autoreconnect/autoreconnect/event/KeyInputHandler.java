fc4a60243e03c5b402fd915aa9f80fd9f0f5467c
MarcoVanHetVista <513496@vistacollege.nl>
2023-06-12 11:18:21 +0200

    ik snap het niet meer jonge eindig deze fucking pijn

--- src/main/java/autoreconnect/autoreconnect/event/KeyInputHandler.java
+++ src/main/java/autoreconnect/autoreconnect/event/KeyInputHandler.java
@@ -12,28 +12,69 @@ public class KeyInputHandler {
     public static final String KEY_CATEGORY_VISTACLIENT =  "key.category.vistaclient";
     public static final String KEY_VISTACLIENT_TESTKEY = "key.category.vistaclient.test";
     public static final String KEY_VISTACLIENT_BUNNYHOP = "key.category.vistaclient.bhop";
+    public static final String KEY_VISTACLIENT_SPEEDHACK = "key.category.vistaclient.speedhack";
+    public static final String KEY_VISTACLIENT_LAUNCH = "key.category.vistaclient.launch";
     public static KeyBinding testkey;
     public static KeyBinding bunnyhop;
+    public static KeyBinding speedhack;
+    public static KeyBinding launch;
     public static boolean BHOPTOGGLE = false;
+    public static boolean SPEEDTOGGLE = false;
+    public static float SPEEDVALUE = 5;
+    public static float SPEEDBUFFER;
+    public static int LAUNCHSPEED = 100;
+    public static boolean LAUNCHTOGGLE = false;
 
     public static void registerKeyInputs() {
         //testkey
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if(testkey.wasPressed()){
+            if (testkey.wasPressed()) {
                 client.player.jump();
             }
         });
+
         //bhop
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if(bunnyhop.wasPressed()){
+            if (bunnyhop.wasPressed()) {
                 BHOPTOGGLE = !BHOPTOGGLE;
                 client.player.sendMessage(Text.of(String.valueOf(BHOPTOGGLE)));
-                if (BHOPTOGGLE && client.player.isOnGround() && client.player.isSprinting()){
+                int i = 0;
+                while (BHOPTOGGLE && client.player.isOnGround() && client.player.isSprinting() && i < 100) {
                     client.player.jump();
+                    i++;
                 }
 //                else {return;}
                 //                bhopLogic.start(); fucjing threads
+            }
+        });
+
+        //speed
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (speedhack.wasPressed()) {
+                SPEEDBUFFER = client.player.speed;
+                SPEEDTOGGLE = !SPEEDTOGGLE;
+                client.player.sendMessage(Text.of(String.valueOf(SPEEDTOGGLE)));
+                client.player.sendMessage(Text.of(String.valueOf(SPEEDVALUE)));
+                client.player.sendMessage(Text.of(String.valueOf(SPEEDBUFFER)));
+                if (SPEEDTOGGLE) {
+                    client.player.speed += SPEEDVALUE;
+                } else {
+                    client.player.speed = SPEEDBUFFER;
                 }
+            }
+        });
+
+        //launch
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (LAUNCHTOGGLE || launch.wasPressed()) {
+                int i = 0;
+                LAUNCHTOGGLE = !LAUNCHTOGGLE;
+                while (i < LAUNCHSPEED && client.player.isOnGround()) {
+                    client.player.setSprinting(true);
+                    client.player.jump();
+                    i++;
+                }
+            }
         });
     }
     public static void register() {
@@ -48,6 +89,18 @@ public class KeyInputHandler {
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_LEFT_ALT, //change for default
                 KEY_CATEGORY_VISTACLIENT));
+
+        speedhack = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEY_VISTACLIENT_SPEEDHACK,
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_V, //change for default
+                KEY_CATEGORY_VISTACLIENT));
         registerKeyInputs();
+
+        launch = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEY_VISTACLIENT_LAUNCH,
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_C, //change for default
+                KEY_CATEGORY_VISTACLIENT));
     }
 }
